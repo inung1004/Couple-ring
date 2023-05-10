c1ecff59981f485e708b1b143db4d96506954eae
inung1004 <dksdbswl10430@naver.com>
2023-05-10 23:38:53 +0900

    Setting✨ 글로벌 스타일 설정

---
+++ src/style/index.ts
@@ -0,0 +1,10 @@
+export const MainColor = "#D4E5FF";
+export const PointColor = "#81B3FF";
+
+export const COLOR = {
+  red: "#FF8181",
+  pink: "#FFD1D1",
+  black: "#423B3B",
+};
+
+export const Font = "'Noto Sans KR', sans-serif;";
